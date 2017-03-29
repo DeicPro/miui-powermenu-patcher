6732922bee0a6046416a7dfb9ab2f41c735f8cb4
DeicPro <DeicPro@users.noreply.github.com>
2017-03-29 15:58:04 +0200

    separate patch code to other file, add update system to download patches, wget is embedded, environment is downloaded

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
-echo "Updating package lists..."
-apt update
-echo "Installing wget..."
-echo y | apt install wget
-echo "Downloading bin.zip"
-wget --no-check-certificate https://raw.githubusercontent.com/DeicPro/miui-powermenu-patcher/bin/bin.zip
+echo "Downloading dependencies..."
+wget --no-check-certificate https://raw.githubusercontent.com/DeicPro/miui-powermenu-patcher/bin/wget.zip
 echo "Building script..."
-base64 bin.zip >> miui-powermenu-patcher.sh
+base64 wget.zip >> miui-powermenu-patcher.sh
 echo "Script built: $(pwd)/miui-powermenu-patcher.sh"
